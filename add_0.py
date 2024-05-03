25bcb487d148d7a33e537fda89f1bcc22c24bd8a
Vijaykumar <hi.vjkr@gmail.com>
2024-05-03 16:47:27 +0530

    Add files via upload

---
+++ add_0.py
@@ -0,0 +1,1 @@
+print("GoldenEgg: Waiting for your PR")
---
+++ main.py
@@ -0,0 +1,22 @@
+import add
+add.run()
+import subtract
+subtract.run()
+import multiply
+multiply.run()
+import divide
+divide.run()
+import AND
+AND.run()
+import OR
+OR.run()
+import NAND
+NAND.run()
+import NOR
+NOR.run()
+import NOT
+NOT.run()
+import XOR
+XOR.run()
+import XNOR
+XNOR.run()
